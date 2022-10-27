96b14b5b4572c574853754baecadd19c181f4b55
user-railcoil <116594300+user-railcoil@users.noreply.github.com>
2022-10-27 15:18:53 -0500

    Update origin.js

--- tests/origin.js
+++ tests/origin.js
@@ -1,7 +1,7 @@
 var 
   html = document.documentElement,
   body = document.querySelector( 'body' ),
-  button = document.querySelector( 'button' );
+  button = document.querySelector( 'div' );
 
 function colorBackground() {
   html.style.display = 'none';
@@ -11,9 +11,39 @@ function colorBackground() {
       'rgb(0, 187, 187)' 
   ){
     body.style.backgroundColor = '#eee';
+    button.style.backgroundColor = '#0bb';
+    document.head.innerHTML = 
+      `
+        <meta charset='UTF-8'>
+        <meta 
+          name='viewport'
+          content=
+            'width=device-width, initial-scale=1.0'
+        >
+        <meta name="theme-color" content="#f07">        
+        <link rel='stylesheet' href='css/base.css'>
+        <link rel='icon' href='favicon.png'>
+        <title>Poop</title>      
+      `;
   }
   else {
     body.style.backgroundColor = '#0bb';
+    button.style.backgroundColor = '#222';
+    button.style.color = 
+      'rgba( 225,225,225,0.75 )';
+    document.head.innerHTML = 
+      `
+      <meta charset='UTF-8'>
+      <meta 
+        name='viewport'
+        content=
+          'width=device-width, initial-scale=1.0'
+      >
+      <meta name="theme-color" content="#07f">        
+      <link rel='stylesheet' href='css/base.css'>
+      <link rel='icon' href='favicon.png'>
+      <title>Poop</title>      
+    `;    
   }
 }
 
