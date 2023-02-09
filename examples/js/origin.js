355176fbb390de18fad169358630c06db528c636
user-railcoil <116594300+user-railcoil@users.noreply.github.com>
2023-02-08 20:40:23 -0600

    Create origin.js

---
+++ examples/js/origin.js
@@ -0,0 +1,24 @@
+const scrollingElement = (document.scrollingElement || document.body);
+
+const scrollToBottom = () => {
+   scrollingElement.scrollTop = scrollingElement.scrollHeight;
+}
+
+const scrollToTop = () => {
+   scrollingElement.scrollTop = 0;
+}
+
+// Require jQuery
+const scrollSmoothToBottom = () => {
+   $(scrollingElement).animate({
+      scrollTop: document.body.scrollHeight,
+   }, 500);
+}
+
+// Require jQuery
+const scrollSmoothToTop = () => {
+   $(scrollingElement).animate({
+      scrollTop: 0,
+   }, 500);
+}
+
