211191f57f619d97157a0e42ab9d8bd121df38bf
user-railcoil <116594300+user-railcoil@users.noreply.github.com>
2023-02-09 13:27:34 -0600

    Update origin.js

--- so/scroll/js/origin.js
+++ so/scroll/js/origin.js
@@ -2,36 +2,30 @@ let
   topButton = document.querySelector( '.top' ),
   bottomButton = document.querySelector( '.bottom' );
 
-// scroll to specific values,
-// same as window.scroll() method.
-// for scrolling a particular distance, use window.scrollBy().
 window.scroll({
   top: 2500, 
   left: 0, 
   behavior: 'smooth' 
 });
 
-// scroll certain amounts from current position 
 window.scrollBy({ 
-  top: 0, // negative value acceptable
+  top: 0, 
   left: 0, 
   behavior: 'smooth' 
 });
 
-
 function scrollToBottom() {
   bottomButton.scrollIntoView({ 
-    behavior: 'smooth' 
+    behavior: 'smooth'
   });
 }
 function scrollToTop() {
   topButton.scrollIntoView({ 
-    behavior: 'smooth' 
+    behavior: 'smooth'
   });
 }
-
 
 topButton
   .addEventListener( 'click', scrollToBottom );
 bottomButton
-  .addEventListener( 'click', scrollToTop );  
+  .addEventListener( 'click', scrollToTop );
